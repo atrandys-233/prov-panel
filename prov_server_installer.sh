d0e17760140fe8ecc2ec834feff0b1fb987f06df
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-09 00:17:10 +0800

    Update prov_server_installer.sh

--- prov_server_installer.sh
+++ prov_server_installer.sh
@@ -42,70 +42,19 @@ cd /etc/v2ray/
 #删除原有v2配置文件
 rm -f config.json
 
-#下载kcp+tcp配置文件，kcp（srtp混淆），tcp（http混淆）
-wget https://raw.githubusercontent.com/yobabyshark/proV/master/config.json
+#下载配置文件
+wget -O config.json https://raw.githubusercontent.com/yobabyshark/prov-panel/master/prov_server_config.json
 
 #生成并替换uuid，kcp、tcp各一个
-kcpuuid=$(cat /proc/sys/kernel/random/uuid)
-tcpuuid=$(cat /proc/sys/kernel/random/uuid)
-sed -i "s/aaaa/$kcpuuid/;s/bbbb/$tcpuuid/;" config.json
+#kcpuuid=$(cat /proc/sys/kernel/random/uuid)
+#tcpuuid=$(cat /proc/sys/kernel/random/uuid)
+#sed -i "s/aaaa/$kcpuuid/;s/bbbb/$tcpuuid/;" config.json
 
 #生成并修改端口
-port=$(rand 10000 30000)
-sed -i "s/11234/$port/" config.json
+#port=$(rand 10000 30000)
+#sed -i "s/11234/$port/" config.json
 
 #重启prov
 systemctl restart v2ray.service
 
-#输出配置到文件
-cat > /etc/v2ray/myconfig.json<<-EOF
-{
-===========KCP配置=============
-地址：${serverip}
-端口：${port}
-uuid：${kcpuuid}
-额外id：64
-加密方式：aes-128-gcm
-传输协议：kcp
-别名：mykcp
-伪装类型：srtp
-===========TCP配置=============
-地址：${serverip}
-端口：${port}
-uuid：${tcpuuid}
-额外id：64
-加密方式：aes-128-gcm
-传输协议：tcp
-别名：mytcp
-伪装类型：http
-伪装域名：bing.com,cloudflare.com,ajax.microsoft.com
-}
-EOF
 
-
-#输出配置信息
-clear
-echo
-echo "安装已经完成，开启了kcp和tcp两种模式，客户端可任意选择对应的配置"
-echo 
-echo "===========KCP配置============="
-echo "地址：${serverip}"
-echo "端口：${port}"
-echo "uuid：${kcpuuid}"
-echo "额外id：64"
-echo "加密方式：aes-128-gcm"
-echo "传输协议：kcp"
-echo "别名：mykcp"
-echo "伪装类型：srtp"
-echo 
-echo "===========TCP配置============="
-echo "地址：${serverip}"
-echo "端口：${port}"
-echo "uuid：${tcpuuid}"
-echo "额外id：64"
-echo "加密方式：aes-128-gcm"
-echo "传输协议：tcp"
-echo "别名：mytcp"
-echo "伪装类型：http"
-echo "伪装域名：bing.com"
-echo
